b251c9b1fd193c906b1a1dda2c6dfd5624a2a36d
David Smallhoover <dsmallhoover@gmail.com>
2022-12-05 19:30:11 -0500

    started on navbar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <nav-bar>
+        <ul class="nav-links">
+          <li><router-link id="home" v-bind:to="{name: 'home'}">Home</router-link></li>
+        </ul>
+      </nav-bar>
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from "./components/NavBar.vue"
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  }
+};
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+  body {
+    font-family: 'Nunito', sans-serif;
+  }
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="home">
+    <nav-bar></nav-bar>
+    <home />  
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
   </div>
 </template>
 
 <script>
+import Home from "../components/Home.vue"
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    Home,
+  }
 };
 </script>
